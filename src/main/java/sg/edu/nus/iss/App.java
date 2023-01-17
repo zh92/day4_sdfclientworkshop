a38b049f3c13886195fdba516dd4bb00fbcf308d
zh92 <zhehao92@gmail.com>
2023-01-17 15:01:33 +0800

    Added client connection

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,17 +1,32 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- */
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public final class App {
-    private App() {
-    }
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("locahost", 12345);
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
+        try(OutputStream os = socket.getOutputStream()) {
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
 
+            Console cons = System.console();
+            String readInput = "";
+
+            while(!readInput.equalsIgnoreCase("close")) {
+                readInput = cons.readLine("Enter a command: ");
+                dos.writeUTF(readInput);
+                dos.flush();
+            }
+
+        } catch (EOFException ex) {
+            socket.close();
+        }
     }
 }
